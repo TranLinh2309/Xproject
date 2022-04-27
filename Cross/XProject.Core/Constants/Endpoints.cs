9e87fd7a606f6b3ddf45f7661a358d3e3765e697
torinoutacnh <54936986+torinoutacnh@users.noreply.github.com>
2022-04-27 10:04:35 +0700

    as

---
+++ Cross/XProject.Core/Constants/Endpoints.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XProject.Core.Constants
+{
+    public class Endpoints
+    {
+        public static class HomeEndpoint
+        {
+            public const string Area = "";
+
+            public const string BaseEndpoint = "~/" + Area;
+        }
+    }
+}
---
+++ Repository/XProject.Repository/Infrastructure/AppDbContext.DbSet.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using XProject.Contract.Repository.Models;
+
+namespace XProject.Repository.Infrastructure
+{
+    public sealed partial class AppDbContext
+    {
+        public DbSet<WorkingTime> WorkingTimes { get; set; }
+    }
+}
